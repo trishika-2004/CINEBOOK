69a3498f4c5c56c3825b392ea5d424121021ed75
Pedda gangi reddy trishika reddy <trishika302@gmail.com>
2026-01-30 12:39:26 +0530

    Add files via upload

---
+++ Backend/src/routes/showRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const {
+  getShows,
+  getShow,
+  createShow,
+  updateShow,
+  deleteShow,
+  getShowSeats
+} = require('../controllers/showController');
+const { protect, authorize } = require('../middleware/auth');
+
+const router = express.Router();
+
+router.route('/')
+  .get(getShows)
+  .post(protect, authorize('admin'), createShow);
+
+router.route('/:id')
+  .get(getShow)
+  .put(protect, authorize('admin'), updateShow)
+  .delete(protect, authorize('admin'), deleteShow);
+
+router.get('/:id/seats', getShowSeats);
+
+module.exports = router;
